6fb8bec5640d9045b9cf2ae346959ca206309f4e
pengliang <pengliang@huihuabao.com>
2018-07-18 18:00:24 +0800

    Frscro

--- app/src/main/java/com/example/administrator/androidtestdemo/activity/FrescoDemoActivity.java
+++ app/src/main/java/com/example/administrator/androidtestdemo/activity/FrescoDemoActivity.java
@@ -1,13 +1,21 @@
 package com.example.administrator.androidtestdemo.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.administrator.androidtestdemo.R;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
 import com.facebook.drawee.generic.GenericDraweeHierarchy;
 import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.request.BasePostprocessor;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.facebook.imagepipeline.request.Postprocessor;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,13 +36,55 @@ public class FrescoDemoActivity extends AppCompatActivity {
     }
 
     private void init() {
-        GenericDraweeHierarchyBuilder builder =
-                new GenericDraweeHierarchyBuilder(getResources());
-        GenericDraweeHierarchy hierarchy = builder
-                .setFadeDuration(300)
-                .build();
-        drawee1.setHierarchy(hierarchy);
+//        GenericDraweeHierarchyBuilder builder =
+//                new GenericDraweeHierarchyBuilder(getResources());
+//        GenericDraweeHierarchy hierarchy = builder
+//                .setFadeDuration(300)
+//                .build();
+//        drawee1.setHierarchy(hierarchy);
         Uri uri = Uri.parse("http://ww2.sinaimg.cn/large/7a8aed7bgw1es8c7ucr0rj20hs0qowhl.jpg");
-        drawee1.setImageURI(uri);
+
+
+
+        Postprocessor redMeshPostprocessor = new BasePostprocessor() {
+            @Override
+            public String getName() {
+                return "redMeshPostprocessor";
+            }
+
+//            @Override
+//            public void process(Bitmap bitmap) {
+//                for (int x = 0; x < bitmap.getWidth(); x+=2) {
+//                    for (int y = 0; y < bitmap.getHeight(); y+=2) {
+//                        bitmap.setPixel(x, y, Color.RED);
+//                    }
+//                }
+//            }
+
+            @Override
+            public void process(Bitmap destBitmap, Bitmap sourceBitmap) {
+                for (int x = 0; x < destBitmap.getWidth(); x++) {
+                    for (int y = 0; y < destBitmap.getHeight(); y++) {
+                        destBitmap.setPixel(destBitmap.getWidth() - x, y, sourceBitmap.getPixel(x, y));
+                    }
+                }
+            }
+        };
+
+        ImageRequest request = ImageRequestBuilder.newBuilderWithSource(uri)
+                .setPostprocessor(redMeshPostprocessor)
+                .build();
+
+        PipelineDraweeController controller = (PipelineDraweeController)
+                Fresco.newDraweeControllerBuilder()
+                        .setImageRequest(request)
+//                        .setOldController(drawee1.getController())
+                        // other setters as you need
+                        .build();
+        drawee1.setController(controller);
+
+//        Uri uri = Uri.parse("res://com.example.administrator.androidtestdemo/"+R.mipmap.back);
+//        drawee1.setImageURI(uri);
     }
 }
+
